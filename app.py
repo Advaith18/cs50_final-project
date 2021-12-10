503046de6860a181048613807ed25a1f247d35fd
Advaith18 <tadvaith18@gmail.com.com>
2021-12-10 14:14:25 +0530

    h

---
+++ app.py
@@ -0,0 +1,73 @@
+from cs50 import SQL
+from flask import Flask, render_template, redirect, session, request, flash
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///finance.db")
+
+@app.route("/", methods = ["POST", "GET"])
+def index():
+    if not session.get("user_id"):
+        return redirect("/login")
+    return render_template("index.html")
+
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_hash = generate_password_hash(password)
+        user = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if not user:
+            return render_template("apology.html", apology="username_not")
+        elif user:
+            rows = db.execute("SELECT id FROM users WHERE username = ?", username)
+            session["user_id"] = rows[0]["id"]
+            return redirect("/")
+    elif request.method == "GET":
+        return render_template("login.html")
+
+
+@app.route("/register", methods = ["POST", "GET"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        if not username:
+            return render_template("apology.html", apology="no_username")
+        elif not password:
+            return render_template("apology.html", apology="no_password")
+        elif not confirmation:
+            return render_template("apology.html", apology="no_confirmation")
+        elif confirmation != password:
+            return render_template("apology.html", apology="no_match")
+        else:
+            password_hashed = generate_password_hash(password)
+            db.execute("INSERT INTO users(username, password_hash) VALUES(?, ?)", username, password_hashed)
+            return redirect("/login")
+
+
+@app.route("/logout", methods = ["POST", "GET"])
+def logout():
+    session.clear()
+    return redirect("/login")
+
+
+@app.route("/beginners", methods = ["POST", "GET"])
+def beginners():
+    if not session.get("user_id"):
+        return redirect("/login")
+    return render_template("beginners.html")
+
+@app.route("/intermediate", methods = ["POST", "GET"])
+def intermediate():
+    return render_template("intermediate.html")
